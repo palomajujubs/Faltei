84fc7879265360c885fba71ae92dfcda6e7a20d0
Paloma <paloma.sousa.santos@hotmail.com>
2024-03-28 15:49:51 -0300

    feat: alterações no api.py

---
+++ api/v1/api.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from  api.v1.endpoints import usuario
+
+
+api_router = APIRouter()
+
+
+api_router.include_router(usuario.router, prefix='/usuarios', tags=['usuarios'])
